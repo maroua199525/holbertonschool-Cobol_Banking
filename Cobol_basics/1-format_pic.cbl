ee18bafa354257a15a6d2f0a21a41627367b378e
maroua199525 <2646@holbertonstudents.com>
2025-04-23 11:55:07 +0100

    add task

---
+++ Cobol_basics/1-format_pic.cbl
@@ -0,0 +1,18 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. FormatPicExample.
+
+       ENVIRONMENT DIVISION.
+
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01 Customer-ID          PIC X(9)        VALUE "CUST00123".
+       01 Account-Balance      PIC 9(5)V99     VALUE 1234.56.
+       01 Interest-Rate        PIC 9(1)V99     VALUE 5.75.
+
+       PROCEDURE DIVISION.
+
+       DISPLAY "Customer ID  : " Customer-ID.
+       DISPLAY "Account Balance : " Account-Balance.
+       DISPLAY "Interest Rate  : " Interest-Rate "%" .
+       
+       STOP RUN.
